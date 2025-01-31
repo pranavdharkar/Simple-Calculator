6c825610b5ace518751057bdfa313eda54049259
Pranav Dharkar <pranavdharkar@gmail.com>
2025-02-01 01:17:47 +0530

    Added Calculation code with switch

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 int main()
 {
+    int num1, num2, result;
+    char op;
 
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the operator (+ - * /): ";
+    cin >> op;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+      result = num1+num2;
+      break;
+    case '-':
+      result = num1-num2;
+      break;
+    case '*':
+      result = num1*num2;
+      break;
+    case '/':
+      result = num1/num2;
+      break;
+    
+    default:
+      break;
+    }
+
+  cout << "Num1 " << op << " Num2 = " << result << endl; 
   return 0;
 }
